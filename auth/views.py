aa2489863274271177fa5ffacc1131014ce50e1e
汪隽立 <juanli-w21@mails.tsinghua.edu.cn>
2023-03-21 20:44:57 +0800

    Initial commit

--- auth/views.py
+++ auth/views.py
@@ -4,4 +4,4 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
 def startup(req: HttpRequest):
-    return HttpResponse("Congratulations! You have successfully installed the requirements. Go ahead!")
+    return HttpResponse("This is a startup page for our IM project")
