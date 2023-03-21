0b55fde4af57acd118e7a03b63512da011c3c3cb
汪隽立 <juanli-w21@mails.tsinghua.edu.cn>
2023-03-21 17:44:21 +0800

    editing urls.py

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ python3 manage.py makemigrations board
 python3 manage.py migrate
 
 uwsgi --module=DjangoHW.wsgi:application \
-    --env DJANGO_SETTINGS_MODULE=DjangoHW.settings \
-    --master \
+    --env DJANGO_SETTINGS_MODULE=IMBackend.settings \
+    --main \
     --http=0.0.0.0:80 \
     --processes=5 \
     --harakiri=20 \
