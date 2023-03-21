0113c4f50a57e43150f4e6fb41ef4ee8681d9f37
Weinan Dai <1826049339@qq.com>
2023-03-22 00:58:58 +0800

    修改了配置以使用MySQL作为数据库

--- IMBackend/settings.py
+++ IMBackend/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'IMBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # 默认
+        'NAME': 'IM',  # 连接的数据库
+        'HOST': 'localhost',  # mysql的ip地址
+        'PORT': 3306,  # mysql的端口
+        'USER': 'root',  # mysql的用户名
+        'PASSWORD': '123456'  # mysql的密码
     }
 }
 
