2a764c0c430f77b58dac8664584f1a0e61cc8f4e
戴炜楠 <2021012958@secoder.net>
2023-03-28 06:21:34 +0000

    fix：修改了数据库ip

--- IMBackend/settings.py
+++ IMBackend/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认
         'NAME': 'IM',  # 连接的数据库
-        'HOST': 'database.KillThisSE.secoder.local',  # mysql的ip地址
+        'HOST': database.KillThisSE.secoder.local,  # mysql的ip地址
         'PORT': 3306,  # mysql的端口
         'USER': 'root',  # mysql的用户名
         'PASSWORD': '123456'  # mysql的密码
