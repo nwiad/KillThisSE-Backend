486294bd5f12f849dbffbdd9c6e1c435c873589c
汪隽立 <juanli-w21@mails.tsinghua.edu.cn>
2023-04-22 21:26:18 +0800

    test: 测试CI

--- IMBackend/settings.py
+++ IMBackend/settings.py
@@ -43,7 +43,7 @@ if os.getenv('DEPLOY'):
         "default": {
             "BACKEND": "channels_redis.core.RedisChannelLayer",
             "CONFIG": {
-                "hosts": [("redis-KillThisSE.app.secoder.net", 6379)],
+                "hosts": [("redis-KillThisSE.app.secoder.net:6379")],
             },
         },
     }
@@ -59,7 +59,7 @@ else:
         "default": {
             "BACKEND": "channels_redis.core.RedisChannelLayer",
             "CONFIG": {
-                "hosts": [("redis-KillThisSE.app.secoder.net", 6379)],
+                "hosts": [("redis-KillThisSE.app.secoder.net:6379")],
             },
         },
     }
