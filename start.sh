a9ce7967d9f38a9f36bfdd0a55649c15d0d1e231
汪隽立 <juanli-w21@mails.tsinghua.edu.cn>
2023-04-24 22:04:21 +0800

    test: 测试CI

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 python3 manage.py makemigrations user
 python3 manage.py migrate
 
-daphne -b 0.0.0.0 -p 80 IMBackend.asgi:application
+# daphne -b 0.0.0.0 -p 80 IMBackend.asgi:application
 
 uwsgi --module=IMBackend.wsgi:application \
     --env DJANGO_SETTINGS_MODULE=IMBackend.settings \
