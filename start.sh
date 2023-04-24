98c4baf0dd9c06394349c5546433a517ae61fe75
汪隽立 <juanli-w21@mails.tsinghua.edu.cn>
2023-04-24 20:47:29 +0800

    test: 测试CI

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ uwsgi --module=IMBackend.asgi:application \
     --env DJANGO_SETTINGS_MODULE=IMBackend.settings \
     --master \
     --http=0.0.0.0:80 \
+    --http-websockets \
     --processes=5 \
     --harakiri=20 \
     --max-requests=5000 \
