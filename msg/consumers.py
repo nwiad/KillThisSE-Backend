f7c3d5bd9a2770c34e71f798440c1f6c8d7f1f80
汪隽立 <juanli-w21@mails.tsinghua.edu.cn>
2023-05-16 01:28:33 +0800

    版本回退

--- msg/consumers.py
+++ msg/consumers.py
@@ -12,7 +12,7 @@ from asgiref.sync import sync_to_async
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.conversation_id = self.scope['url_route']['kwargs']['conversation_id']
-        self.user_id = self.scope['url_route']['kwargs']['user_id']
+        # self.user_id = self.scope['url_route']['kwargs']['user_id']
         await self.channel_layer.group_add(str(self.conversation_id), self.channel_name)
         await self.channel_layer.group_send(str(self.conversation_id), {"type": "chat_message"})
         await self.accept()
